f1215847af9bc64785c8a709bc82bfc5ebf40ebb
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-06 00:57:32 +0300

    third Commit

--- app3/Program.cs
+++ app3/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections;
 namespace app3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Üçüncü uygulamaya Hoşgeldiniz.");
+            Console.WriteLine("Lütfen bir cümle giriniz.");
+
+            string[] cumle = Console.ReadLine().Split(" ");
+            char[] sesliHarfler = {'a','e','ı','i','o','ö','u','ü'};
+            ArrayList liste = new ArrayList();
+            foreach(string s in cumle){
+                foreach (char c in s ){
+                    if (sesliHarfler.Contains(c)){
+                        liste.Add(c);
+                    }
+                }
+            }
+            liste.Sort();
+            char[] sesliHarflerArrayi = new char[liste.Count];
+            for(int i =0;i<liste.Count;i++){
+                sesliHarflerArrayi[i]=Convert.ToChar(liste[i]);
+            }
+
+            foreach(char ch in sesliHarflerArrayi){
+                Console.WriteLine(ch);
+            }
+            
+
         }
     }
 }
